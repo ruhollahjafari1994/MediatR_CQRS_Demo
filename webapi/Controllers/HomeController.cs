d90e75304640473d09bac2b5c2be1844e6d6ac29
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2021-09-09 10:55:55 +0430

    Add project files.

---
+++ webapi/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoLibrary;
+using MediatR;
+using DemoLibrary.Models;
+using DemoLibrary.Handlers;
+using DemoLibrary.Commands;
+ using DemoLibrary.Queries;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IMediator _mediator;
+            public HomeController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+        // GET: api/<HomeController>
+        [HttpGet]
+        public async Task<List<PersonModel>> Get()
+        {
+            return await _mediator.Send(new GetPersonListQuery()); 
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public async Task<PersonModel> Get(int id)
+        {
+            return await _mediator.Send(new GetPersonByIdQuery(id));
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
